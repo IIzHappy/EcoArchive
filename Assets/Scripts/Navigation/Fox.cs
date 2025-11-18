f0017e662fb65915694d447ddf00c8d34ca266f8
KaiIC5343 <jacob.hyland@ontariotechu.net>
2025-11-18 14:52:40 -0500

    chasing mechanics concept

--- Assets/Scripts/Navigation/Fox.cs
+++ Assets/Scripts/Navigation/Fox.cs
@@ -1,17 +1,28 @@
 using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class Fox : MonoBehaviour
 {
-
+    public float chaseDistance;
+    public Transform foxPosition;
     private GameObject[] foxWaypoints;
     private NavMeshAgent agent;
     private GameObject NavDestination;
     private Quaternion Rotation;
+    private GameObject[] prey;
+    private GameObject chasePrey;
+    [SerializeField] bool isChasing = false;
+    [SerializeField] float chaseTime = 10f;
+    [SerializeField] float chaseTimer = 0f;
+    [SerializeField] float chaseRecharge = 10f;
+    public bool chaseRecharging = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        foxPosition = transform;
+        prey = GameObject.FindGameObjectsWithTag("Prey");
         foxWaypoints = GameObject.FindGameObjectsWithTag("FoxPoints");
         Debug.Log(foxWaypoints);
         Debug.Log(foxWaypoints.Length);
@@ -25,11 +36,49 @@ public class Fox : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rotation = Quaternion.Euler(0, +90f, 0);
-       // gameObject.transform.LookAt(agent.destination + Rotation);
-        if (agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance <= agent.stoppingDistance)
+        if(chaseRecharging == true)
         {
-            newWaypoint();
+            chaseRecharge -= Time.deltaTime;
+            if(chaseRecharge <= 0f)
+            {
+                chaseRecharge = 10f;
+                chaseRecharging = false;
+            }
+        }
+        foreach (GameObject target in prey)
+        {
+            if (target != null && !isChasing && !chaseRecharging) {
+                float distance = Vector3.Distance(foxPosition.position, target.transform.position);
+                if (distance < chaseDistance && !isChasing)
+                {
+                    isChasing = true;
+                    chasePrey = target;
+                    Debug.Log("isChasing");
+
+                }
+            }
+        }
+        Rotation = Quaternion.Euler(0, +90f, 0);
+        if (!isChasing)
+        {
+            // gameObject.transform.LookAt(agent.destination + Rotation);
+            if (agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                newWaypoint();
+            }
+        }
+        else
+        {
+            Chase();
+            chaseTimer += Time.deltaTime;
+            if (chaseTimer >= chaseTime)
+            {
+                isChasing = false;
+                chaseRecharging = true;
+                chaseTimer = 0;
+                newWaypoint();
+                Debug.Log("AbandonChase");
+            } 
         }
     }
 
@@ -40,5 +89,10 @@ public class Fox : MonoBehaviour
         agent.destination = NavDestination.transform.position;
         Debug.Log(foxWaypoints[index]);
         Debug.Log(agent.destination);
+    }
+
+    void Chase()
+    {
+        agent.SetDestination(chasePrey.transform.position);
     }
 }
