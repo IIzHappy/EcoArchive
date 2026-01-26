92ba2814fdc9b34dd4c61f9c1388bc692aa1cf35
KaiIC5343 <jacob.hyland@ontariotechu.net>
2026-01-26 17:03:06 -0500

    movement update
    basis for roaming overhaul, needs more work before implementation into main scene.

---
+++ Assets/Scripts/Navigation/PreyNav.cs
@@ -0,0 +1,47 @@
+using System.Runtime.CompilerServices;
+using Unity.Hierarchy;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PreyNav : MonoBehaviour
+{
+    [SerializeField] float sampleRadius = 10f;
+    [SerializeField] float sampleInterval; 
+    [SerializeField] int maxSampleAttempts = 10;
+    private NavMeshAgent agent;
+    private float moveTimer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        sampleInterval = Random.Range(2f, 6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveTimer += Time.deltaTime;
+        if(moveTimer >= sampleInterval && !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            Move();
+            moveTimer = 0f;
+            sampleInterval = Random.Range(2f, 6f);
+        }
+    }
+
+    private void Move()
+    {
+      for(int i = 0; i < maxSampleAttempts; i++)
+        {
+            Vector3 randDirection = Random.insideUnitSphere * sampleRadius;
+            randDirection += transform.position;
+            randDirection.y = 0f;
+
+            if(NavMesh.SamplePosition(randDirection, out NavMeshHit hit, sampleRadius, NavMesh.AllAreas))
+            {
+                agent.SetDestination(hit.position);
+                return;
+            }
+        }
+    }
+}
