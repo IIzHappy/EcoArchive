01d138dfc44e54c361fe88d9c3fc37a85c946611
IIzHappy <racheldong8@gmail.com>
2026-02-02 19:55:31 -0500

    fixed scenechanger

--- Assets/Scripts/SceneManagers/SceneChanger.cs
+++ Assets/Scripts/SceneManagers/SceneChanger.cs
@@ -3,22 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public static SceneChanger Instance { get; private set; }
+
     [SerializeField] string _mainMenu;
 
     //maybe adding smth that tells other scripts the scene changed
 
     private void Awake()
     {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
         DontDestroyOnLoad(gameObject);
     }
 
-    void LoadMainMenu()
+    public void LoadMainMenu()
     {
         SceneManager.LoadScene(_mainMenu);
     }
 
-    void LoadScene(string sceneName)
+    public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
