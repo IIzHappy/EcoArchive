0a82ab7366bcdd26838b597e2e14750ec498c321
IIzHappy <racheldong8@gmail.com>
2026-03-02 19:42:26 -0500

    scene buttons work

--- Assets/Scripts/SceneManagers/SceneChanger.cs
+++ Assets/Scripts/SceneManagers/SceneChanger.cs
@@ -3,28 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
-    public static SceneChanger Instance { get; private set; }
-
-    [SerializeField] string _mainMenu;
-
-    //maybe adding smth that tells other scripts the scene changed
-
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
-
-    public void LoadMainMenu()
-    {
-        SceneManager.LoadScene(_mainMenu);
-    }
-
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
