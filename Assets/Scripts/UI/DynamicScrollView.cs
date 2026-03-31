e10c738f959beadc5b240476f66b12af14bdf5fc
IIzHappy <racheldong8@gmail.com>
2026-03-31 15:33:40 -0400

    photo scrolling

---
+++ Assets/Scripts/UI/DynamicScrollView.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DynamicScrollView : MonoBehaviour
+{
+    [SerializeField] Transform _content;
+    [SerializeField] GameObject _prefab;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
