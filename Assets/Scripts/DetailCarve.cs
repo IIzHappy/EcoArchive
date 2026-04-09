bd465660f7427a702640535f29b358fac31c60da
PotatoSpyware <seansteven7@icloud.com>
2026-04-09 10:43:53 -0400

    Making detail czarve more versatile

--- Assets/Scripts/DetailCarve.cs
+++ Assets/Scripts/DetailCarve.cs
@@ -18,73 +18,72 @@ public class DetailCarve : MonoBehaviour
     [ContextMenu("Extract")]
     public void Extract()
     {
-        Debug.Log(terrain.terrainData.detailPatchCount);
         Bounds test;
         Collider[] colliders = terrain.GetComponentsInChildren<Collider>();
 
-        //Skip the first, since its the Terrain Collider
         for (int i = 1; i < colliders.Length; i++)
         {
-            //Delete all previously created colliders first
             DestroyImmediate(colliders[i].gameObject);
         }
 
-        //for (int i = 0; i < terrain.terrainData.treePrototypes.Length; i++)
-        //{
-            DetailPrototype detail = terrain.terrainData.detailPrototypes[2];
+        for (int l = 0; l < terrain.terrainData.detailPrototypes.Length; l++)
+        {
+            DetailPrototype detail = terrain.terrainData.detailPrototypes[l];
 
-            //Get all instances matching the prefab index
-            //DetailInstanceTransform[] instances = terrain.terrainData.ComputeDetailInstanceTransforms();
-            //treeInstances.Where(x => x.prototypeIndex == i).ToArray();
-
-            for (int j = 0; j < 32; j++)
+            for (int j = 0; j < terrain.terrainData.detailPatchCount; j++)
             {
-            for (int k = 0; k < 32; k++)
-            {
-
-                DetailInstanceTransform[] instances = terrain.terrainData.ComputeDetailInstanceTransforms(j, k, 2, 1, out test);
-                //Un-normalize positions so they're in world-space
-                for (int i = 0; i < instances.Length; i++)
+                for (int k = 0; k < terrain.terrainData.detailPatchCount; k++)
                 {
-                    Vector3 position = new Vector3(instances[i].posX, instances[i].posY, instances[i].posZ);
-                    //position = Vector3.Scale(position, terrain.terrainData.size);
-                    position += terrain.GetPosition();
+                    DetailInstanceTransform[] instances = terrain.terrainData.ComputeDetailInstanceTransforms(j, k, l, 1, out test);
+                    for (int i = 0; i < instances.Length; i++)
+                    {
+                        Vector3 position = new Vector3(instances[i].posX, instances[i].posY, instances[i].posZ);
+                        position += terrain.GetPosition();
 
-                    //Fetch the collider from the prefab object parent
-                    CapsuleCollider prefabCollider = detail.prototype.GetComponent<CapsuleCollider>();
+                        CapsuleCollider prefabCollider = detail.prototype.GetComponent<CapsuleCollider>();
 
-                    if (!prefabCollider) continue;
+                        if (!prefabCollider) continue;
 
-                    GameObject obj = new GameObject();
-                    obj.name = detail.prototype.name + j + k;
-                    obj.transform.rotation = Quaternion.Euler(0, instances[i].rotationY * Mathf.Rad2Deg, 0);
-                    Vector3 scale = new Vector3(instances[i].scaleXZ, instances[i].scaleY, instances[i].scaleXZ);
-                    obj.transform.localScale = scale;
-                    //obj.transform.rotation.SetEulerAngles(0, instances[i].rotationY, 0);
-                    //obj.transform.rotation.SetAxisAngle(Vector3.up, instances[i].rotationY * Mathf.Rad2Deg);
+                        GameObject obj = new GameObject();
+                        obj.name = detail.prototype.name + j + k;
+                        obj.transform.rotation = Quaternion.Euler(0, instances[i].rotationY * Mathf.Rad2Deg, 0);
+                        Vector3 scale = new Vector3(instances[i].scaleXZ, instances[i].scaleY, instances[i].scaleXZ);
+                        obj.transform.localScale = scale;
 
-                    CapsuleCollider objCollider = obj.AddComponent<CapsuleCollider>();
+                        CapsuleCollider objCollider = obj.AddComponent<CapsuleCollider>();
 
-                    objCollider.center = prefabCollider.center;
-                    objCollider.height = prefabCollider.height;
-                    objCollider.radius = prefabCollider.radius;
-                    objCollider.direction = prefabCollider.direction;
+                        objCollider.center = prefabCollider.center;
+                        objCollider.height = prefabCollider.height;
+                        objCollider.radius = prefabCollider.radius;
+                        objCollider.direction = prefabCollider.direction;
 
-                    NavMeshObstacle obstacle = obj.AddComponent<NavMeshObstacle>();
-                    obstacle.shape = NavMeshObstacleShape.Box;
-                    obstacle.center = prefabCollider.center;
-                    obstacle.size = new Vector3 (prefabCollider.radius * 2, prefabCollider.radius * 2 , prefabCollider.height);
-                    obstacle.carving = true;
+                        NavMeshObstacle obstacle = obj.AddComponent<NavMeshObstacle>();
+                        obstacle.shape = NavMeshObstacleShape.Box;
+                        obstacle.center = prefabCollider.center;
+                        switch (objCollider.direction)
+                        {
+                            case 0:
+                                obstacle.size = new Vector3(prefabCollider.height, prefabCollider.radius * 2, prefabCollider.radius * 2);
+                                break;
+                            case 1:
+                                obstacle.size = new Vector3(prefabCollider.radius * 2, prefabCollider.height, prefabCollider.radius * 2);
+                                break;
+                            case 2:
+                                obstacle.size = new Vector3(prefabCollider.radius * 2, prefabCollider.radius * 2, prefabCollider.height);
+                                break;
+                            default:
+                                Debug.Log("No Direction");
+                                break;
+                        }
+                        obstacle.carving = true;
 
+                        obj.layer = terrain.gameObject.layer;
 
-                    //if (terrain.preserveTreePrototypeLayers) obj.layer = detail.prototype.layer;
-                    obj.layer = terrain.gameObject.layer;
-
-                    obj.transform.position = position;
-                    obj.transform.parent = terrain.transform;
+                        obj.transform.position = position;
+                        obj.transform.parent = terrain.transform;
+                    }
                 }
             }
-            }
-        //}
+        }
     }
 }
